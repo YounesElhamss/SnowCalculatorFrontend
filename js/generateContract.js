75f863cc49e1f8bc050885466f4b9ba6d62bae2c
Younes Elhamss <youneselhamss@gmail.com>
2023-01-10 10:12:33 +0100

    MVP

---
+++ js/generateContract.js
@@ -0,0 +1,43 @@
+const hej = "hej";
+const a1 = document.getElementById("a1");
+const blob1 = new Blob([hej], {type: "application/msword"})
+a1.href = URL.createObjectURL(blob1);
+
+//Sørg for at det man skriver i input er det der bliver lagt i word
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
